c04490490b6599107120556c7c8b70d271bfd907
Ngan LE <ngan.ltkn2911@gmail.com>
2022-10-31 17:18:47 +0700

    Call API for product feature

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,44 @@
-function getProductInfo() {
-    
+(function getProductInfo() {
+    var promise = axios({
+        url: 'https://shop.cyberlearn.vn/api/Product',
+        method: 'GET',
+        
+    });
+
+    promise.then(function(res) {
+        console.log(res.data.content);
+
+        renderProductItem(res.data.content);
+    })
+
+    promise.catch(function(err) {
+        console.log(err);
+    })
+})();
+
+// Render product 
+function renderProductItem(arrProduct) {
+    var contentHTML = '';
+
+    for (var i = 0; i < arrProduct.length; i++) {
+        var product = arrProduct[i];
+
+        contentHTML += `
+            <div class="product-item">
+                <div class="product-img">
+                    <img class="w-100" height="350px" src="${product.image}" />
+                </div>
+                <div class="product-desc">
+                    <h5>${product.name}</h5>
+                    <p>${product.description.length > 50 ? product.description.substr(0, 50) + " ..." : product.description}</p>
+                </div>
+                <div class="product-btn">
+                    <a href="#" class="btn-buy">Buy now</a>
+                    <a href="#" class="btn-price">${product.price}$</a>
+                </div>
+            </div>
+        `;
+    }
+
+    document.querySelector('#productList').innerHTML = contentHTML;
 }
